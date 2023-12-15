8681a4e0b3905ce43c260ed783e3981b953079a5
Ian Jones <jonesi_mail@yahoo.co.uk>
2023-12-15 11:33:02 +0000

    Added bike types

--- components/types.ts
+++ components/types.ts
@@ -19,3 +19,34 @@ export interface Filters {
   minPrice: number,
   maxPrice: number
 }
+
+export interface BikeSizeData {
+  [key: string] : string[]
+}
+
+type ForkTravelOptions = Record<string, string[]>;
+type WheelSizesOptions = Record<string, string[]>;
+
+export interface Bike {
+  brand: string;
+  price: number;
+  bike_id: number;
+  category: string;
+  created_at: string;
+  model_name: string;
+  model_year: number | null;
+  fork_travel: ForkTravelOptions[];
+  wheel_sizes: WheelSizesOptions[];
+  bike_alt_ref: string;
+  model_family: string;
+  frame_material: string;
+};
+
+export interface BikeFilterSelection {
+  header: string,
+  selection: string[]
+}
+
+export interface BikeFilterState {
+  [key: string]: string[];
+};
