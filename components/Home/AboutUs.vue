83e1f9fdd2e5da9841e0a4bae9584c4a13982aa4
Ian Jones <jonesi_mail@yahoo.co.uk>
2023-12-22 13:05:10 +0000

    Bike related home page

--- components/Home/AboutUs.vue
+++ components/Home/AboutUs.vue
@@ -3,15 +3,11 @@
     <div class="row p-4">
       <!-- TODO there should an h1 title here -->
       <div class="col-6">
-        <img class="float-lg-right" src="~/assets/new.jpg" alt="home-img" title="home-img" loading="lazy">
+        <img class="float-lg-right" :src="useAsset('home/tom-austin-XbZy0-JHINA-unsplash.jpg')" alt="home-img" title="home-img" loading="lazy">
       </div>
       <div class="col-12 col-sm-6">
-        <h2>New minimalist<br>–components</h2>
-        <p>Gravity will force your feet to the ground. Don’t be afraid, your mind will find your freedom.
-          You won’t miss a sunrise. You’ll rise with the sun.
-          <br>You’ll never run away from the storm. You will flow with the wind.
-          <br>Don’t hesitate to pause. Follow the flow of the moment.
-          And then, take the first step towards the new you.
+        <h2>Supa dupa Bike Shop Site!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur consequat libero. Duis nibh est, auctor in tincidunt non, iaculis sit amet libero. Fusce vitae varius turpis. Integer consectetur sapien a elit condimentum condimentum. Etiam vel libero libero. Ut pharetra convallis nibh, eu accumsan erat pellentesque ut. Cras scelerisque est quis dui suscipit posuere. Donec facilisis odio urna, quis molestie sapien gravida et. Phasellus sit amet varius turpis. Nam diam arcu, finibus blandit tempus eu, fringilla eu tortor. Cras vel euismod nisl, vitae placerat lacus. Morbi iaculis, lacus in convallis fringilla, mauris lectus laoreet lacus, egestas consequat diam eros ut nisl.
         </p>
       </div>
     </div>
@@ -19,16 +15,12 @@
       <div
         class="col-12 col-sm-6 text-sm-right text-md-right ml-sm-0 ml-md-5 ml-lg-5 ml-xl-5 text-lg-right order-sm-first order-12"
         style="margin-right: -8%; z-index:1;">
-        <h2>Save Space<br>more freedom</h2>
-        <p>Gravity will force your feet to the ground. Don’t be afraid, your mind will find your freedom.
-          You won’t miss a sunrise. You’ll rise with the sun.
-          You’ll never run away from the storm. You will flow with the wind.
-          Don’t hesitate to pause. Follow the flow of the moment.
-          And then, take the first step towards the new you.
+        <h2>Workshop Facilities</h2>
+        <p>Nulla porta facilisis scelerisque. Nam nec mattis dolor, in placerat lectus. Donec lobortis fringilla neque nec feugiat. Aenean ac erat neque. Vivamus vestibulum nec risus nec fringilla. Nulla facilisi. Sed sed ante a sapien elementum vulputate. Nulla dictum velit sit amet mi eleifend fringilla. Nam vestibulum ipsum sit amet sapien pretium, et pharetra dui venenatis. Donec sed eleifend sapien.
         </p>
       </div>
       <div class="col-6">
-        <img src="~/assets/33.jpg" alt="home-img-2" title="home-img-2" loading="lazy">
+        <img :src="useAsset('home/taylor-smith-GNSIOmiBFaY-unsplash.jpg')" alt="home-img-2" title="home-img-2" loading="lazy">
       </div>
     </div>
   </section>
--- components/Home/Carousel.vue
+++ components/Home/Carousel.vue
@@ -1,25 +1,28 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <section class="carousel">
-    <div id="heroControls" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#heroControls" data-bs-slide-to="0" class="active" aria-current="true"
-          aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#heroControls" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#heroControls" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="img-fluid" src="~/assets/jum1.jpg" alt="First-slide" title="First-slide">
+    <section class="carousel">
+        <div id="heroControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#heroControls" data-bs-slide-to="0" class="active" aria-current="true"
+                    aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#heroControls" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#heroControls" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="img-fluid" :src="useAsset('home/pexels-cristiana-raluca-686230.jpg')" alt="First-slide" title="First-slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" :src="useAsset('home/pexels-jose-ricardo-barraza-morachis-3642482.jpg')" alt="Second-slide" title="Second-slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" :src="useAsset('home/nick-wood-sd7TynEcu18-unsplash.jpg')" alt="Third-slide" title="Third-slide">
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-          <img class="img-fluid" src="~/assets/jum2.jpg" alt="Second-slide" title="Second-slide">
-        </div>
-        <div class="carousel-item">
-          <img class="img-fluid" src="~/assets/jum3.jpg" alt="Third-slide" title="Third-slide">
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <style scoped>
@@ -37,5 +40,14 @@
 
 .carousel-indicators>.active {
   background: #e0f0f8;
+}
+
+@media (max-width: 768px ) {
+    .carousel-inner {
+        width:800px;
+        position:relative;
+        left:50%;
+        margin-left:-400px;
+    }
 }
 </style>
